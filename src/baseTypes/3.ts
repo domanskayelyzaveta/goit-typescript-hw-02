e35cd37bcf412742dacd4c2119c5d8e5b77730af
domanskayelyzaveta <domanskaya_@ukr.net>
2023-10-23 11:11:15 +0200

    push for push

--- src/baseTypes/3.ts
+++ src/baseTypes/3.ts
@@ -1,6 +1,21 @@
-let some:unknown;
+// let some: any;
+// some = 'Text';
+// let str: string;
+// str = some;
+
+// export { };
+
+////////////////////
+
+let some: unknown;
 some = 'Text';
 let str: string;
-str = some;
 
-export {};
+
+if (typeof some === "string") {
+    str = some;
+} else {
+    console.error("Not a string");
+}  
+
+export { };
